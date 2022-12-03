a4d9685d35fa9f7b394567c287892dbaf8c79957
Sébastien <sebastien.mariaux@protonmail.com>
2022-12-03 15:23:53 +0100

    Day 3

---
+++ day_3/1.py
@@ -0,0 +1,32 @@
+import re
+
+
+def import_data(sample=False):
+    dataset = 'sample_data.txt' if sample else 'data.txt'
+    f = open(dataset, "r")
+    data = f.read()
+    return data
+
+ITEMS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+prios = {item: i+1 for i, item in enumerate(ITEMS)}
+
+def split_2(text):
+    half =  len(text) // 2
+    return text[:half], text[half:]
+
+def solve(data):
+    stacks = data.split("\n")
+    sum = 0
+    for stack in stacks:
+        c1, c2 = split_2(stack)
+        common = set(c1).intersection(set(c2))
+        sum +=  prios[list(common)[0]]
+    print(sum)
+
+def main():
+    solve(import_data(True))
+    solve(import_data(False))
+
+
+if __name__ == '__main__':
+    main()
---
+++ day_3/2.py
@@ -0,0 +1,31 @@
+import re
+
+
+def import_data(sample=False):
+    dataset = 'sample_data.txt' if sample else 'data.txt'
+    f = open(dataset, "r")
+    data = f.read()
+    return data
+
+
+ITEMS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+prios = {item: i+1 for i, item in enumerate(ITEMS)}
+
+
+def solve(data):
+    stacks = data.split("\n")
+    sum = 0
+    for i in range(0, len(stacks), 3):
+        stack = stacks[i:i+3]
+        common = [el for el in stack[0] if el in stack[1] and el in stack[2]]
+        sum += prios[common[0]]
+    print(sum)
+
+
+def main():
+    solve(import_data(True))
+    solve(import_data(False))
+
+
+if __name__ == '__main__':
+    main()
