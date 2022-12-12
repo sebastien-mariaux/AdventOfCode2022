df258066e2a518971f7e6e4e0c633a7d2353c5ef
Sébastien <sebastien.mariaux@protonmail.com>
2022-12-12 17:14:20 +0100

    works but too expensive

--- day_12/1.py
+++ day_12/1.py
@@ -24,34 +24,49 @@ def adjacents(point, data, nb_col, nb_row):
             and VALUES[data[i][j]] <= value + 1
             ]
 
-def get_counts(point, counts, data, current_count, nb_col, nb_row, visited):
-    visited.append(point)
-    for adj in [a for a in adjacents(point, data, nb_col, nb_row) if a not in visited]:
-        next_count = current_count
-        next_visited = visited
-        if adj[2] == 'E':
-            # print(adj)
-            next_count += 1
-            counts.append(next_count)
-        else:
-            next_count += 1
-            next_count += get_counts(adj, counts, data, next_count, nb_col, nb_row, next_visited)[1]
-        return counts, next_count
+# def get_counts(point, counts, data, current_count, nb_col, nb_row, visited):
+#     visited.append(point)
+#     for adj in [a for a in adjacents(point, data, nb_col, nb_row) if a not in visited]:
+#         next_count = current_count
+#         next_visited = visited
+#         if adj[2] == 'E':
+#             # print(adj)
+#             next_count += 1
+#             counts.append(next_count)
+#         else:
+#             next_count += 1
+#             next_count += get_counts(adj, counts, data, next_count, nb_col, nb_row, next_visited)[1]
+#         return counts, next_count
 
 
 def solve(data):
     data = [list(el) for el in data.split('\n')]
     nb_col = len(data[0])
     nb_row = len(data)
-    counts = []
+    count = 0
     S = [(i, j, 'S', 0) for j in range(nb_col)
          for i in range(nb_row) if data[i][j] == 'S'][0]
-    print(get_counts(S, counts, data, 0, nb_col, nb_row, []))
+    found = False
+    routes = [[S]]
+    while not found:
+        count += 1
+        print(count)
+        new_routes = []
+        for route in routes:
+            for adj in adjacents(route[-1], data, nb_col, nb_row):
+                if adj[2] == 'E':
+                    found = True
+                    break
+                if adj not in route:
+                    new_route  = route
+                    new_routes.append(new_route + [adj])
+        routes = new_routes
+    print(count)
 
 
 def main():
     solve(import_data(True))
-    # solve(import_data(False))
+    solve(import_data(False))
 
 
 if __name__ == '__main__':
