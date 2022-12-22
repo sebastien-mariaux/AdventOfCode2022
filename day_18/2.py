201d22f34fef6432b3db16018ddb6d24a6fedce7
Sébastien <sebastien.mariaux@protonmail.com>
2022-12-22 19:15:54 +0100

    complete day 18

--- day_18/2.py
+++ day_18/2.py
@@ -25,7 +25,6 @@ def neighbors(cube):
 def solve(data):
     cubes = list(map(lambda x: tuple([int(i)
                  for i in x.split(',')]), data.splitlines()))
-    faces = 0
     adjacents = {cube: 6 for cube in cubes}
     for cube in cubes:
         for n in neighbors(cube):
@@ -41,34 +40,47 @@ def solve(data):
     min_z = min([z for x, y, z in cubes])
     max_z = max([z for x, y, z in cubes])
 
-    air_cubes = {}
-
+    air_cubes = []
+    not_air_cubes = []
     all_cubes = [(x, y, z) for x in range(min_x-1, max_x+2)
                  for y in range(min_y-1, max_y+2) for z in range(min_z-1, max_z+2)]
     for cb in all_cubes:
+        is_air_cube = True
         if cb not in cubes:
-            is_air_cube = True
-            if not [1 for x in range(cb[0], max_x+1) if (x, cb[1], cb[2]) in cubes]:
+            if not [1 for x in range(cb[0], max_x+2) if (x, cb[1], cb[2]) in cubes]:
                 is_air_cube = False
-            elif not [1 for x in range(min_x, cb[0]) if (x, cb[1], cb[2]) in cubes]:
+            elif not [1 for x in range(min_x-1, cb[0]+2) if (x, cb[1], cb[2]) in cubes]:
                 is_air_cube = False
-            elif not [1 for y in range(cb[1], max_y+1) if (cb[0], y, cb[2]) in cubes]:
+            elif not [1 for y in range(cb[1], max_y+2) if (cb[0], y, cb[2]) in cubes]:
                 is_air_cube = False
-            elif not [1 for y in range(min_y, cb[1]) if (cb[0], y, cb[2]) in cubes]:
+            elif not [1 for y in range(min_y-1, cb[1]+2) if (cb[0], y, cb[2]) in cubes]:
                 is_air_cube = False
-            elif not [1 for z in range(cb[2], max_z+1) if (cb[0], cb[1], z) in cubes]:
+            elif not [1 for z in range(cb[2], max_z+2) if (cb[0], cb[1], z) in cubes]:
                 is_air_cube = False
-            elif not [1 for z in range(min_z, cb[2]) if (cb[0], cb[1], z) in cubes]:
+            elif not [1 for z in range(min_z-1, cb[2]+2) if (cb[0], cb[1], z) in cubes]:
                 is_air_cube = False
 
             if is_air_cube:
-                air_cubes[cb] = 6
+                air_cubes.append(cb)
+            else:
+                not_air_cubes.append(cb)
 
-    for ac in air_cubes:
-        for n in neighbors(ac):
+    while not_air_cubes:
+        nac = not_air_cubes.pop()
+        N = neighbors(nac)
+        for n in N:
             if n in air_cubes:
-                air_cubes[ac] -= 1
-    return surface - sum(air_cubes.values())
+                air_cubes.remove(n)
+                not_air_cubes.append(n)
+
+    cubes += air_cubes
+    adjacents = {cube: 6 for cube in set(cubes)}
+    for cube in cubes:
+        for n in neighbors(cube):
+            if n in cubes:
+                adjacents[cube] -= 1
+
+    return sum(adjacents.values())
 
 
 def main():
@@ -85,4 +97,4 @@ def test_sample():
 
 
 def test_real():
-    assert solve(import_data(False)) == 4320
+    assert solve(import_data(False)) == 2456
