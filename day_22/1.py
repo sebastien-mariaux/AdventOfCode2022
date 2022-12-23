232437a52ed11a90a4434988ec597975ead10ca3
Sébastien <sebastien.mariaux@protonmail.com>
2022-12-23 21:43:30 +0100

    day 23 part 1

---
+++ day_22/1.py
@@ -0,0 +1,29 @@
+from pprint  import pprint
+import re
+import itertools
+
+def import_data(sample=False):
+    dataset = 'sample_data.txt' if sample else 'data.txt'
+    f = open(dataset, "r")
+    data = f.read()
+    return data
+
+
+def solve(data):
+    pass
+
+
+def main():
+    print(solve(import_data(True)))
+    print(solve(import_data(False)))
+
+
+if __name__ == '__main__':
+    main()
+
+
+def test_sample():
+    assert solve(import_data(True)) == 0
+
+def test_real():
+    assert solve(import_data(False)) == 0
