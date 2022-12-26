745640113265a3ddbc7cf5ae50e380a7cc7e7b85
Sébastien <sebastien.mariaux@protonmail.com>
2022-12-26 18:22:26 +0100

    Day 24 fail

--- day_24/1.py
+++ day_24/1.py
@@ -24,89 +24,94 @@ def print_map(M, col_num, row_num):
     print()
 
 
-def run_minute(M, col_num, row_num, end, minute):
-    print(f'==== Minute {minute} ====')
-    print_map(M, col_num, row_num)
-    new_M = defaultdict(list)
-    for position, elts in M.copy().items():
-        next_position = position
-        for el in elts:
-            if el == '>':
-                next_position = (position[0], position[1]+1)
-                if M[next_position] == ['#']:
-                    next_position = (position[0], 1)
-            elif el == '<':
-                next_position = (position[0], position[1]-1)
-                if M[next_position] == ['#']:
-                    next_position = (position[0], col_num-2)
-            elif el == '^':
-                next_position = (position[0]-1, position[1])
-                if next_position[0] < 0:
-                    next_position = (row_num-2, position[1])
-                if M[next_position] == ['#']:
-                    next_position = (row_num-1, position[1])
-                if M[next_position] == ['#']:
-                    next_position = (row_num-2, position[1])
-            elif el == 'v':
-                next_position = (position[0]+1, position[1])
-                if next_position[0] == row_num:
-                    next_position = (0, position[1])
-                if M[next_position] == ['#']:
-                    next_position = (0, position[1])
-                if M[next_position] == ['#']:
-                    next_position = (1, position[1])
-            new_M[next_position].append(el)
+def get_new_position(M, col_num, row_num, position, el):
+    next_position = position
+    if el == '>':
+        next_position = (position[0], position[1]+1)
+        if M[next_position] == ['#']:
+            next_position = (position[0], 1)
+    elif el == '<':
+        next_position = (position[0], position[1]-1)
+        if M[next_position] == ['#']:
+            next_position = (position[0], col_num-2)
+    elif el == '^':
+        next_position = (position[0]-1, position[1])
+        if next_position[0] < 0:
+            next_position = (row_num-2, position[1])
+        if M[next_position] == ['#']:
+            next_position = (row_num-1, position[1])
+        if M[next_position] == ['#']:
+            next_position = (row_num-2, position[1])
+    elif el == 'v':
+        next_position = (position[0]+1, position[1])
+        if next_position[0] == row_num:
+            next_position = (0, position[1])
+        if M[next_position] == ['#']:
+            next_position = (0, position[1])
+        if M[next_position] == ['#']:
+            next_position = (1, position[1])
+    return next_position
 
-        # Elf moves
-    cp = next(k for k,v in M.items() if 'E' in v)
-    print('cp', cp)
-    adjacents = [
-            (cp[0]+1, cp[1]),
-            (cp[0], cp[1]+1),
-            (cp[0], cp[1]),
-            (cp[0]-1, cp[1]),
-            (cp[0], cp[1]-1),
-        ]
-    adjacents = [el for el in adjacents if el[0] >= 0 and el[0] < row_num and el[1] >= 0 and el[1] < col_num]
-    valid_adj = [el for el in adjacents if new_M[el] == []]
 
-    print('valid next positions', valid_adj)
-    if end in valid_adj:
-        print_map(M, col_num, row_num)
-        return M, True
-    if not valid_adj:
-        pass
-    if len(valid_adj) == 1:
-        new_M[valid_adj[0]].append('E')
-        new_M[cp].remove('E')
-    if len(valid_adj) > 1:
-        new_M[valid_adj[0]].append('E')
-        new_M[cp].remove('E')
-    M = new_M.copy()
 
-    return M, False
+
+
+
 
 
 
 def solve(data):
     M = {(i,j): [] if el == '.' else [el] for i, row in enumerate(data.splitlines()) for j, el in enumerate(row)}
     M[(0,1)] = ['E']
-    print(M)
 
     col_num = len(data.splitlines()[0])
     row_num = len(data.splitlines())
     last_row = data.splitlines()[-1]
     end = next((row_num-1, i) for i, el in enumerate(last_row) if  el == '.')
+    totals = set()
 
+    def run_minute(M, col_num, row_num, end, minute):
+        if minute > 50:
+            return
+        print(f'==== Minute {minute} ====')
+        print(totals)
+        # print_map(M, col_num, row_num)
+        new_M = defaultdict(list)
+        for position, elts in M.copy().items():
+            for el in elts:
+                next_position = get_new_position(M, col_num, row_num, position, el)
+                new_M[next_position].append(el)
 
+        # Elf moves
+        cp = next(k for k,v in M.items() if 'E' in v)
+        print('cp', cp)
+        # print_map(M, col_num, row_num)
 
-    for minute in range(1000):
-        print(minute)
-        M, finished = run_minute(M, col_num, row_num, end, minute)
-        if finished:
-            return minute
+        adjacents = [
+                (cp[0]+1, cp[1]),
+                (cp[0], cp[1]+1),
+                (cp[0], cp[1]),
+                (cp[0]-1, cp[1]),
+                (cp[0], cp[1]-1),
+            ]
+        adjacents = [el for el in adjacents if el[0] >= 0 and el[0] < row_num and el[1] >= 0 and el[1] < col_num]
+        valid_adj = [el for el in adjacents if new_M[el] in [[], ['E']]]
+        # if not valid_adj:
+        #     pass
+        if end in valid_adj:
+            totals.add(minute)
+        else:
+            for valid in valid_adj:
+                next_M = new_M.copy()
+                print_map(next_M, col_num, row_num)
+                breakpoint()
+                next_M[valid].append('E')
+                next_M[cp].remove('E')
+                run_minute(next_M, col_num, row_num, end, minute+1)
 
-
+    run_minute(M, col_num, row_num, end, 1)
+    print(totals)
+    return min(totals)
 
 def main():
     print(solve(import_data(True)))
